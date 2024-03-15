bf670e1782a37760ccb94133c0f1df6e083c3c52
xhcherry <cyhcherry@outlook.com>
2024-03-15 18:02:32 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-<picture>
-  <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/xhcherry/homepage/output/github-contribution-grid-snake-dark.svg">
-  <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/xhcherry/homepage/output/github-contribution-grid-snake.svg">
-  <img alt="github contribution grid snake animation" src="https://raw.githubusercontent.com/xhcherry/homepage/output/github-contribution-grid-snake.svg">
-</picture>
+
+![](https://raw.githubusercontent.com/xhcherry/homepage/output/github-contribution-grid-snake.svg)
+
+![](https://raw.githubusercontent.com/xhcherry/homepage/output/github-contribution-grid-snake-dark.svg)
--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,5 @@
-console.log('%cCopyright © 2024 zyyo.net', 'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;');
-console.log('%c   /\\_/\\', 'color: #8B4513; font-size: 20px;');
-console.log('%c  ( o.o )', 'color: #8B4513; font-size: 20px;');
-console.log(' %c  > ^ <', 'color: #8B4513; font-size: 20px;');
-console.log('  %c /  ~ \\', 'color: #8B4513; font-size: 20px;');
-console.log('  %c/______\\', 'color: #8B4513; font-size: 20px;');
+console.log('%cCopyright © 2021 xherry.com', 'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;');
+console.log('%c 1', 'color: #8B4513; font-size: 20px;');
 
 document.addEventListener('contextmenu', function (event) {
     event.preventDefault(); // 阻止默认的上下文菜单行为
@@ -48,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (theme == "Dark") {
             themeState = "Dark";
             changeSvg("#ffffff");
-            tanChiShe.src = "https://raw.githubusercontent.com/xhcherry/homepage/output/github-contribution-grid-snake-dark.svg";
+            tanChiShe.src = "https://raw.githubusercontent.com/xhcherry/homepage/output/github-contribution-grid-snake.svg";
             htmlTag.dataset.theme = 'dack';
         } else if (theme == "Blue") {
             themeState = "Blue";
